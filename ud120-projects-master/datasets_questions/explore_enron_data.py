008131bdacac2c0865893d21b4fb9c73d4f8b148
Steven Greulich <greulich.steven@gmail.com>
2018-06-20 05:22:17 -0700

    Commit - Initial Data Analysis
    The purpose of this commit is for the following analysis that has been performed:
    1) how many rows in the set
    2) how many features are there for each row
    3) how many people are labeled as a person of interest (POI)

--- ud120-projects-master/datasets_questions/explore_enron_data.py
+++ ud120-projects-master/datasets_questions/explore_enron_data.py
@@ -19,4 +19,24 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+#Print how many rows we have in the data set
+print('Number of rows: ')
+print(len(enron_data))
+
+#print how many features are there for each row
+max_count = 0
+print('Features: ')
+for key,value in enron_data.items():
+    temp_count = len(list(filter(bool, value)))
+    if max_count < temp_count:
+        max_count = temp_count
+
+print(max_count)
+
+#Print out how many people are labeled as a Person of Interest (POI)
+for key,value in enron_data.items():   
+    if max_count < temp_count:
+        max_count = temp_count
+
+print('Number of people labeled as a POI: ')
 
